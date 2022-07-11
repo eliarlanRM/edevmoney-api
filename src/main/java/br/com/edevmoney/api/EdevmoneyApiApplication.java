5d169cf1643b5310b78877b0f728a3abae235d5a
Eliarlan Rodrigues <eliarlanrodrigues7@gmail.com>
2022-07-11 14:31:54 -0300

    first commit

---
+++ src/main/java/br/com/edevmoney/api/EdevmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.edevmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdevmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdevmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edevmoney/api/EdevmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edevmoney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdevmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
